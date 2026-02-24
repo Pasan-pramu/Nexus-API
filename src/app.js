b214b24b962129b3c84a8b78a246ff70343f4cdf
Pasan-pramu <pramudithapasan076@gmail.com>
2026-02-24 23:03:34 +0530

    implement Eslint &  Prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Nexus');
-})
+  res.status(200).send('Hello from Nexus');
+});
 
 export default app;
